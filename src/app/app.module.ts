a4507fd9cadc927b8dc12a19195c01f45d22fc61
Vignesh-R-3kt <vignesh.r@3ktechnologies.com>
2023-11-30 14:55:17 +0530

    changes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-import { SearchComponent } from './component/search/search.component';
+import { SearchComponent } from './components/search/search.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 import { HttpClientModule } from "@angular/common/http";
 
