8cb0739f95c479da9d631a5f55918265310fc937
Vignesh-R-3kt <vignesh.r@3ktechnologies.com>
2023-11-30 16:09:01 +0530

    added movie details tab

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiServicesService } from 'src/app/services/api-services.service';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.scss']
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit {
+
+  movieDetailsResult: any;
+  movieVideoResult: any;
+  movieCastResult: any;
+
+  constructor(private http: ApiServicesService, private router: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    const movieId = this.router.snapshot.paramMap.get("id");
+    this.getMovie(movieId);
+    this.getVideo(movieId);
+    this.getMovieCast(movieId);
+  }
+
+  getMovie(id: any) {
+    this.http.getMovieDetails(id).subscribe((res: any) => {
+      this.movieDetailsResult = res;
+    })
+  }
+
+  getVideo(id: any) {
+    this.http.getMovieVideo(id).subscribe((res: any) => {
+      res.results.forEach((ele: any) => {
+        if (ele.type === 'Trailer') {
+          this.movieVideoResult = ele.key;
+        }
+      })
+      console.log(this.movieVideoResult);
+    })
+  }
+
+  getMovieCast(id: any) {
+    this.http.getMovieCast(id).subscribe((res: any) => {
+      console.log("cast", res.cast);
+      this.movieCastResult = res.cast;
+    })
+  }
 
 }
